fbe7febad6a55455a7f1111ee4220fa6aaf83485
Thales Menezes <thalesmenezes286@gmail.com>
2024-05-05 10:48:18 -0300

    Verifica se é um ano bissexto entre os anos de 1900 até 2020

---
+++ ano-bissexto.py
@@ -0,0 +1,20 @@
+
+#Função verifica se é um ano bissexto retornando True 
+def verifica_ano_bissexto(ano):
+    """ 
+     Verificar se um ano é bissexto usando uma função simples. 
+     Um ano bissexto é aquele que é divisível por 4, exceto para os anos que são divisíveis por 100, 
+     a menos que sejam divisíveis por 400.
+    """
+
+    if(ano % 4 == 00 and ano % 100 != 0) or (ano % 400 == 0):
+        return True;
+    else:
+        return False;
+
+#Loop For percorre os anos de 1900 até 2020 e imprime os anos bissextos
+for ano in range(1900, 2021):
+
+    if verifica_ano_bissexto(ano):
+        print(f"{ano} é um ano bissexto.")
+    
